a4881a74bba15116eea7959b5fef87647ade8836
Keshan Peiris <keshan.peiris@gmail.com>
2016-02-16 16:14:33 +1100

    added followers and following

---
+++ app/mirage/fixtures/users.js
@@ -0,0 +1,11 @@
+export default [
+  { 
+    id: 1, 
+    username: 't4t5',
+    aboutMe: 'I like making stuff.',
+    joinedAt: new Date('2015-06-08T09:30:26'),
+    followees: [1],
+    followers: [1],
+    chirps: [1, 2, 3] // <-- Add the "3"
+  }
+];
